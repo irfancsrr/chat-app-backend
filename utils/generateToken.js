a32304a241757ecabe0982d676f4082bfc9399b8
irfan <irfancsrrsimt@gmail.com>
2026-02-06 18:15:44 +0530

    chat-app-backend

---
+++ utils/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "15d",
+	});
+
+	res.cookie("jwt", token, { httpOnly: true, secure: false,  sameSite: "lax",
+		maxAge: 15 * 24 * 60 * 60 * 1000,
+	 });
+};
+
+export default generateTokenAndSetCookie;
